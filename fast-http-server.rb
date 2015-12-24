42e4ccdcedbc257d404b1071ee22346337200dc6
Sdogruyol <dogruyolserdar@gmail.com>
2015-12-24 19:44:31 +0200

    Update

--- fast-http-server.rb
+++ fast-http-server.rb
@@ -1,7 +1,7 @@
 class FastHttpServer < Formula
   desc "Super fast, zero configuration command line HTTP Server."
   homepage "https://github.com/sdogruyol/fast-http-server"
-  url "https://github.com/sdogruyol/fast-http-server/archive/0.1.0.zip"
+  url "https://github.com/sdogruyol/homebrew-fast-http-server/archive/0.1.0.zip"
   version "0.1.0"
   sha256 "01ef34647bc17bc6dfe255430dbaa5ae50b28e20cba64dc6407677483562b996"
 
