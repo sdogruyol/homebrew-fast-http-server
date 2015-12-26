0259fba837022506e0d63d12828c9750a8789342
Sdogruyol <dogruyolserdar@gmail.com>
2015-12-26 15:18:03 +0200

    Fixed SHA

--- fast-http-server.rb
+++ fast-http-server.rb
@@ -3,7 +3,8 @@ class FastHttpServer < Formula
   homepage "https://github.com/sdogruyol/fast-http-server"
   url "https://github.com/sdogruyol/homebrew-fast-http-server/archive/0.2.0.zip"
   version "0.2.0"
-  sha256 "221ff50cb21079ac930d4feee6f308ac6c904548a776ea979da8782cfb7b2604"
+  sha256 "449fc0b2536925b50a5bd028355f6294c1d91a937e6a35f74a94a03c192588de"
+  head "https://github.com/sdogruyol/fast-httpserver.git"
 
   def install
     bin.install "fast-http-server"
